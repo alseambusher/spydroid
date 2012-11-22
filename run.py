991462ff633fa46ed6cd2bb7d307df9d6a4b339e
Suresh Alse <alseambusher@gmail.com>
2012-11-22 22:20:13 +0530

    final

---
+++ run.py
@@ -0,0 +1,24 @@
+#!/bin/env python
+import gtk,os,gobject
+class spy(gtk.Window):
+	def __init__(self):
+		super(spy,self).__init__()
+		self.set_title("Spydroid")
+		self.set_position(gtk.WIN_POS_CENTER)
+		self.set_border_width(2)
+
+		self.image=gtk.Image()
+		os.system("chmod +x screenshot.jar")
+
+		self.connect("destroy",gtk.main_quit)
+		self.add(self.image)
+		os.system("adb start-server")
+		self.image.set_from_file("1.png")
+		self.show_all()
+		gobject.timeout_add(10,self.update)
+	def update(self):
+		os.system("./screenshot.jar -d spy.png>/dev/null")
+		self.image.set_from_file("spy.png")
+		gobject.timeout_add(10,self.update)
+spy()
+gtk.main()
